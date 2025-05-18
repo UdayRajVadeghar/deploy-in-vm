d9fe4f0b86d6bcc7a4b79224a819bf828272c278
Uday <udayraj.vadeghar@gmail.com>
2025-05-18 13:21:15 +0000

    first commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+const PORT = 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send(
+    "Hello from the virtual machine"
+  );
+});
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
